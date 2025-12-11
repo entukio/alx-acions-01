fabb18f2899348018b1a8819fa582b268b23b235
Adam Jurkiewicz Pythonista Local <adam@jurkiewicz.tech>
2025-12-11 18:32:05 +0100

    test actions

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, Flask! Error with test"
+
+@app.route("/test")
+def hello_world2():
+    return "<h1>Hello, World -  test!</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    rv = client.get('/')
+    assert b'Hello, Flask!' in rv.data
